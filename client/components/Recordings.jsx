8373b3215ca32de7b5b4dbc17f53dc4b464bf344
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-09-10 01:48:14 +0000

    Created an input for the title of the recording and created a basic template on the form is submitted than the audio data will be recorded and sent to the database

--- client/components/Recordings.jsx
+++ client/components/Recordings.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-
 export default class Recordings extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       recording: false,
-      recordings: []
+      recordings: [],
+      formInputs: false,
+      title: '',
+      length: '00:32'
     };
     this.saveAudio = this.saveAudio.bind(this);
     this.deleteAudio = this.deleteAudio.bind(this);
     this.startRecording = this.startRecording.bind(this);
     this.stopRecording = this.stopRecording.bind(this);
+    // this.saveAudioToProfile = this.saveAudioToProfile.bind(this);
   }
 
   async componentDidMount() {
@@ -30,22 +33,38 @@ export default class Recordings extends React.Component {
     e.preventDefault();
     this.chunks = [];
     this.mediaRecorder.start(10);
-    this.setState({ recording: true });
+    this.setState({ recording: true, formInputs: false });
   }
 
   stopRecording(e) {
     e.preventDefault();
     this.mediaRecorder.stop();
-    this.setState({ recording: false });
+    this.setState({ recording: false, formInputs: true });
     this.saveAudio();
   }
 
   saveAudio() {
     const blob = new Blob(this.chunks, { type: 'audio/webm' });
     const audioURL = URL.createObjectURL(blob);
-    const recordings = this.state.recordings.concat([audioURL]);
-    this.setState({ recordings });
+    // To save multiple audio files
+    // const recordings = this.state.recordings.concat([audioURL]);
+    this.setState({ recordings: [audioURL], formButtons: false });
+
   }
+
+  // saveAudioToProfile(){
+  //   const userId = 1;
+  //   const fileName = title.split(' ').join('-');
+  //   const title = this.state.title;
+  //   const length = this.state.length;
+  //   const url = `/voice/${fileName}`;
+
+  //   const params = [userId, fileName, title, length];
+  //   const sql = `
+  //   insert into "recordings" ("userId", "url", "title", "recordingLength")
+  //   values ($1, $2, $3, $4)
+  //   `
+  // }
 
   deleteAudio(audioUrl) {
     const recordings = this.state.recordings.filter(audio => audio !== audioUrl);
@@ -53,7 +72,12 @@ export default class Recordings extends React.Component {
   }
 
   render() {
-    const { recordings, recording } = this.state;
+    const { recordings, recording, formInputs } = this.state;
+    // const submitButtons = (
+    //   <div>
+    //     <button onSubmit={this.superSaveAudio}></button>
+    //   </div>
+    // )
     const recordInner = (
             <div className="outer-record-shell row justify-center-all">
               <div className="inner-record-shell">
@@ -63,23 +87,30 @@ export default class Recordings extends React.Component {
     const buttonClassName = 'col-100 outline record-button row justify-center-all';
 
     return (
-      <>
+      <form action="submit">
+          <div className="row justify-center-all">
+            {formInputs &&
+            <div className="padding-input">
+            <input type="text" className="title-input" />
+            </div>
+            }
+          </div>
         <div className="row justify-center-all padding-record">
           {!recording && <button onClick={e => this.startRecording(e)} className={buttonClassName}>{recordInner}</button>}
           {recording && <button onClick={e => this.stopRecording(e)} className={buttonClassName}>{recordInner}</button>}
         </div>
         <div className="row justify-center-all padding-record">
-          {recordings.map(audioURL => (
-            <div key={audioURL}>
-              <audio controls="controls">
-                <source src={audioURL} />
-              </audio>
-            </div>
-          )
-          )}
+        {recordings.map(audioURL => (
+          <div key={audioURL}>
+            <audio controls="controls">
+              <source src={audioURL} />
+            </audio>
+          </div>
+        )
+        )}
         </div>
+      </form>
 
-      </>
     );
   }
 }
