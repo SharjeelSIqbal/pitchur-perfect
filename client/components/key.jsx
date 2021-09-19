84bbf2bfec2764f5e9e2111e32e99ea8442cc309
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-09-18 17:08:07 -0700

    piano doesn't sound as bad as before, fixed some styling issues where the borders where extremely visible now and not very distinct.

--- client/components/key.jsx
+++ client/components/key.jsx
@@ -12,31 +12,32 @@ export default class Keys extends React.Component {
   }
 
   playKey(e) {
-    const duration = 1;
+    const time = 1;
     const audioContext = new AudioContext();
     const oscillator = audioContext.createOscillator();
-
+    const masterGain = audioContext.createGain();
     const gainNode = audioContext.createGain();
-    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
-    gainNode.gain.linearRampToValueAtTime(0.6, audioContext.currentTime + 0.05);
-    gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime + duration);
+
     oscillator.type = 'triangle';
     oscillator.frequency.value = this.props.note.frequency;
     oscillator.connect(gainNode);
-    gainNode.connect(audioContext.destination);
-
+    gainNode.connect(masterGain);
+    masterGain.connect(audioContext.destination);
+    masterGain.gain.setValueAtTime(1, audioContext.currentTime);
+    gainNode.gain.setValueAtTime(0, audioContext.currentTime);
+    gainNode.gain.linearRampToValueAtTime(0.6, audioContext.currentTime + 0.1);
+    gainNode.gain.linearRampToValueAtTime(0, audioContext.currentTime + time);
     oscillator.start(audioContext.currentTime);
-    oscillator.stop(audioContext.currentTime + duration);
-
+    oscillator.stop(audioContext.currentTime + time);
   }
 
   render() {
-
     const key = this.props.note.note.length === 1 ? 'key' : 'key flat';
+    const perfectKeys = this.props.note.note === 'C' || this.props.note.note === 'F' ? 'no-border-left' : null;
     return (
 
-      <div className={`${key} font-pair`} name={this.props.note.frequency} id={`${this.props.note.note}${this.props.note.octave}`} onClick={this.playKey}>
-        <h3 className="absolute note-to-key ">{this.props.note.note.length === 1 ? this.props.note.note : null}</h3>
+      <div className={`${key} ${perfectKeys} font-pair`} name={this.props.note.frequency} id={`${this.props.note.note}${this.props.note.octave}`} onClick={this.playKey}>
+        <h3 className="absolute note-to-key ">{this.props.note.note.length === 1 ? `${this.props.note.note}${this.props.note.octave}` : null}</h3>
        </div>
 
     );
