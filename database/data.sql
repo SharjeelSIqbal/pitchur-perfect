2124db92540013915301ac6ef02227d0be9748d8
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-09-24 00:25:53 +0000

    removed some data sql so it doesn't mess anything up

--- database/data.sql
+++ database/data.sql
@@ -2,8 +2,6 @@ insert into "users" ("username", "hashPassword")
 values ('Sharjello', 'fakeHashPassword1');
 
 
-insert into "recordings" ("userId", "url", "title", "recordingLength")
-values (1, '/voice/voice-recordings-1', 'first-attempt', '01:30');
 
 insert into "favorite" ("userId", "recordingId")
 values(1, 1);
