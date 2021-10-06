e293003b11dbb04f67e98f2710af41f554d675ac
Sharjeel Iqbal <sharjeelsiqbal@gmail.com>
2021-10-06 05:14:13 +0000

    added basics of sign-up feature

---
+++ client/pages/sing-up.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SignUp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return null;
+  }
+
+}
